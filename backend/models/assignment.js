65785f137f8ff1ef95b7d2f71a0658373506a7b5
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-04-06 00:53:25 -0700

    updated form

--- backend/models/assignment.js
+++ backend/models/assignment.js
@@ -5,14 +5,16 @@ const assignmentSchema = new mongoose.Schema(
     studentName: {
       type: String,
       required: true,
+      trim: true,
     },
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     description: {
       type: String,
-      requird: true,
+      trim: true,
     },
   },
   {
@@ -20,6 +22,4 @@ const assignmentSchema = new mongoose.Schema(
   }
 );
 
-const Assignment = mongoose.model("Assignment", assignmentSchema);
-
-export default Assignment;
+module.exports = mongoose.model("Submission", assignmentSchema);
