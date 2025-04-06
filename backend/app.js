f3911e4c6c84c80ecf6000f5d080c398aa26302a
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-04-05 23:57:43 -0700

    created port setup for backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
+
+// creating express app
+const app = express();
+
+// it handles parsing for the incoming request mainly bodies
+app.use(bodyParser.json());
+
+// initializing cors
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET, POST, PUT, DELETE, HEAD, OPTIONS",
+  })
+);
+
+// initize ports
+const port = process.env.PORT || 600;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
