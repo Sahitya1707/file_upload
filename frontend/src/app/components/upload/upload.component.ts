1bde9e7fef41764636e7e4635b00c5f04b729819
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-04-07 06:50:58 -0400

    updated angular

--- frontend/src/app/components/upload/upload.component.ts
+++ frontend/src/app/components/upload/upload.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './upload.component.html',
-  styleUrl: './upload.component.css'
+  styleUrl: './upload.component.css',
 })
 export class UploadComponent {
+  constructor() {}
+  // creating the type for upload
+  uploadData: {
+    studentName: string;
+    title: string;
+    description: string;
+  } = {
+    studentName: '',
+    title: '',
+    description: '',
+  };
 
+  // creating data types for file
+
+  selectedFile: File | null = null;
+
+  onFileSelected(event: any) {
+    console.log(event);
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      console.log('Selected file:', file);
+    }
+  }
+
+  // creating a function that is needed to submit data
+  onSubmit(): void {
+    const formData = new FormData();
+    formData.append('studentName', this.uploadData.studentName);
+    formData.append('title', this.uploadData.title);
+    formData.append('description', this.uploadData.description);
+
+    if (this.selectedFile) {
+      formData.append('file', this.selectedFile);
+    }
+
+    // if you want to console what the form Data has inside it before send it then
+    for (let pair of formData.entries()) {
+      console.log(`${pair[0]}:`, pair[1]);
+    }
+  }
 }
